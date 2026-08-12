cd3e660ca2e26e94493203eaa06be237dc29aac2
aroo-stack <aroo-stack@users.noreply.github.com>
2026-08-12 10:36:44 +1000

    auto-sync 2026-08-12 10:36:44

---
+++ src-tauri/target/debug/build/tauri-runtime-wry-d465406c4082a2b0/build_script_build-d465406c4082a2b0.d
@@ -0,0 +1,5 @@
+/Users/aroo/Downloads/gesture-tracker-app/src-tauri/target/debug/build/tauri-runtime-wry-d465406c4082a2b0/build_script_build-d465406c4082a2b0.d: /Users/aroo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-runtime-wry-2.11.4/build.rs
+
+/Users/aroo/Downloads/gesture-tracker-app/src-tauri/target/debug/build/tauri-runtime-wry-d465406c4082a2b0/build_script_build-d465406c4082a2b0: /Users/aroo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-runtime-wry-2.11.4/build.rs
+
+/Users/aroo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tauri-runtime-wry-2.11.4/build.rs:
