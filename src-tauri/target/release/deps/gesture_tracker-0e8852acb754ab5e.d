363d7439fd3ab66bfbba70fd7021aadf1a624246
aroo-stack <aroo-stack@users.noreply.github.com>
2026-08-12 13:31:44 +1000

    auto-sync 2026-08-12 13:31:44

---
+++ src-tauri/target/release/deps/gesture_tracker-0e8852acb754ab5e.d
@@ -0,0 +1,5 @@
+/Users/aroo/Downloads/gesture-tracker-app/src-tauri/target/release/deps/gesture_tracker-0e8852acb754ab5e.d: src/main.rs
+
+/Users/aroo/Downloads/gesture-tracker-app/src-tauri/target/release/deps/gesture_tracker-0e8852acb754ab5e: src/main.rs
+
+src/main.rs:
